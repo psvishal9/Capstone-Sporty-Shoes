98be89376794dca983f5af0f091d58a7d381f7e4
Pavuluri Vishal <100855306+psvishal9@users.noreply.github.com>
2024-01-27 19:50:52 +0530

    Added Cucumber Test

---
+++ Cucumber-Test/src/main/java/com/simplilearn/cucumber/testrunner/TestRunner.java
@@ -0,0 +1,10 @@
+package com.simplilearn.cucumber.testrunner;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/Features", glue = { "com/simplilearn/cucumber/stepdefinitions" })
+public class TestRunner {
+}
